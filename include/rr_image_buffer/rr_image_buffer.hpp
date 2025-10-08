2021b121634a3bb4a45cb84c3102f0cf3a36e286
Aaron Spiteri <azzmosphere@gmail.com>
2025-10-08 16:47:50 +1100

    added basic message for retrieving images

--- include/rr_image_buffer/rr_image_buffer.hpp
+++ include/rr_image_buffer/rr_image_buffer.hpp
@@ -5,4 +5,35 @@
 
 namespace rr_image_buffer {
 
+/**
+ * @class RrImageBuffer
+ * @brief image camera buffering service.
+ * 
+ * When images are streamed, provides buffering service. 
+ */
+class RrImageBuffer : public rclcpp::Node
+{
+public:
+    RrImageBuffer() : Node("camera_buffer") {
+        init();
+    }
+
+    /**
+     * @fn init()
+     * @brief perform node inilization.
+     */
+    void init();
+private:
+    /**
+     * @fn image_callback
+     * @brief called when new image is recieved.
+     */
+    void image_callback(const sensor_msgs::msg::Image::SharedPtr msg);
+
+    /**
+     * @fn info_callback
+     * @brief called when new camera info is recieved
+     */
+    void info_callback(const sensor_msgs::msg::CameraInfo::SharedPtr msg);
+};
 }
